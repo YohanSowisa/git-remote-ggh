814f356d0fe19e4eca3798a363dc58c0dfa77ff4
YohanSowisa <sowisayohanes@gmail.com>
2024-01-12 15:53:53 +0000

    menambah file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Coba GIT Remote | GIT to GitHub</title>
+</head>
+<body>
+  <h1>Halo, Coba GIT Remote.</h1>
+</body>
+</html>
